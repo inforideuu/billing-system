1c5a26f7e91a40580572e1e0d2b9c3accb0ef8b5
Annamalai2382 <annamalairaja2002@gmail.com>
2026-08-06 16:56:46 +0530

    update default fallback port to 4000 for TiDB Cloud Serverless

--- retail_billing/settings.py
+++ retail_billing/settings.py
@@ -92,7 +92,7 @@ DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASSWORD = os.environ.get('DB_PASSWORD')
 DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT', '3306')
+DB_PORT = os.environ.get('DB_PORT', '4000')
 DB_SSL_CA = os.environ.get('DB_SSL_CA')
 
 # If DB_SSL_CA is not set, check local path, certifi, or standard Linux system paths
