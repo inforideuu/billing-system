bd29a198a8e87cccdbe15b19b6a09199e12b26ad
Annamalai2382 <annamalairaja2002@gmail.com>
2026-08-06 16:59:11 +0530

    use pymysql driver on Windows to bypass native TLS decryption bugs

--- retail_billing/__init__.py
+++ retail_billing/__init__.py
@@ -1,2 +1,8 @@
 # Zenelait Infotech Retail Billing System
 # TiDB / MySQL is utilized as the primary database backend.
+
+import os
+if os.name == 'nt':
+    import pymysql
+    pymysql.install_as_MySQLdb()
+
